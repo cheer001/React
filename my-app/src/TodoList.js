dd4f9e41e997f6ed35926bbdea0553dd49726e6f
gerry <gerrycheer@gmail.com>
2021-05-09 21:49:13 +0700

    feat:使用antd实现Todolist页面布局

--- my-app/src/TodoList.js
+++ my-app/src/TodoList.js
@@ -1,16 +1,35 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
+import 'antd/dist/antd.css';
+import { Input, Button, List, Typography } from "antd";
+
+const data = [
+    'Racing car sprays burning fuel into crowd.',
+    'Japanese princess to wed commoner.',
+    'Australian walks 100km after outback crash.',
+    'Man charged over missing wedding girl.',
+    'Los Angeles battles huge wildfires.',
+];
 
 class TodoList extends Component {
 
-    // 最优先执行的函数
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         return (
-            <Fragment>
-            </Fragment>
+            <div style={{ marginTop: 10, marginLeft: 10 }}>
+                <div>
+                    <Input placeholder="todo info" style={{ width: 300, marginRight: 10 }} />
+                    <Button type="primary">提交</Button>
+                </div>
+                <List
+                    style={{ marginTop: 10 }}
+                    bordered
+                    dataSource={data}
+                    renderItem={item => (
+                        <List.Item>
+                            <Typography.Text mark>[ITEM]</Typography.Text> {item}
+                        </List.Item>
+                    )}
+                />
+            </div>
         )
     }
 }
