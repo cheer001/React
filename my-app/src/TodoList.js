220fd806e76a663bc75d021c68adf0ad5645c631
gerry <gerrycheer@gmail.com>
2021-05-09 21:36:48 +0700

    开始重构学习项目为 antd+react+redux

--- my-app/src/TodoList.js
+++ my-app/src/TodoList.js
@@ -1,98 +1,16 @@
 import React, { Component, Fragment } from "react";
-import TodoItem from "./TodoItem";
-import "./style.css";
-import axios from "axios";
-import App from "./App";
 
 class TodoList extends Component {
 
     // 最优先执行的函数
     constructor(props) {
         super(props);
-        // 当组件的state或者props发生改变的时候，render函数就会重新执行
-        /** 管理组件的状态 */
-        this.state = {
-            /** input输入框中的值 */
-            inputValue: '',
-            /** 列表中的每一项 */
-            list: [],
-        }
-
-        // 使用ES6语法中的bind函数改变this的指向
-        this.handleInputChange = this.handleInputChange.bind(this);
-        this.handleBtnClick = this.handleBtnClick.bind(this);
-        this.handleItemDelete = this.handleItemDelete.bind(this);
     }
 
     render() {
-        const { inputValue } = this.state;
         return (
             <Fragment>
-                <div>
-                    <label htmlFor="insertArea">输入内容：</label>
-                    <input
-                        id="insertArea"
-                        className="input"
-                        onChange={this.handleInputChange}
-                        value={inputValue}
-                    />
-                    <button onClick={this.handleBtnClick}>提交</button>
-                </div>
-                <ul>
-                    {this.getTodoItem()}
-                </ul>
-                <App />
             </Fragment>
-        )
-    }
-
-    componentDidMount() {
-        axios.get("http://mengxuegu.com:7300/mock/60964e83c7b7385be0a82e32/reactstudy/getTodoList")
-            .then((resp) => {
-                this.setState(() => ({
-                    list: [...resp.data]
-                }))
-            })
-            .catch(() => {
-                console.log("error");
-            });
-    }
-
-    handleInputChange(e) {
-        this.setState(() => {
-            const inputValue = e.target.value;
-            return { inputValue }
-        })
-    }
-    handleBtnClick() {
-        // precState: 修改前的数据
-        this.setState((prevState) => {
-            return {
-                list: [...prevState.list, prevState.inputValue],
-                inputValue: ""
-            }
-        })
-    }
-    handleItemDelete(index) {
-        this.setState((prevState) => {
-            const list = [...prevState.list];
-            list.splice(index, 1);
-            return { list }
-        })
-    }
-
-    getTodoItem() {
-        return (
-            this.state.list?.map((item, index) => {
-                return (
-                    <TodoItem
-                        key={index}
-                        content={item}
-                        index={index}
-                        deleteItem={this.handleItemDelete}
-                    />
-                )
-            })
         )
     }
 }
